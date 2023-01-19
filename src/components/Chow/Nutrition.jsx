63d8f25c40de8ee31d9f9e0e05ee8eccf949fc78
Jude Bobinihi <boblitejude@gmail.com>
2023-01-19 16:00:29 +0200

    feat(chows): move UI into dumb component

--- src/components/Chow/Nutrition.jsx
+++ src/components/Chow/Nutrition.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Text, View } from "react-native";
-export const Nutrition = () => {
+export const Nutrition = ({ NutritionText }) => {
   return (
     <View style={{ flex: 1 }}>
-      <Text>Nutrition</Text>
+      <Text>{NutritionText}</Text>
     </View>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { CustomButton } from "./CustomButton.jsx";
 export { SlideDots } from "./SlideDots";
+export { Chow } from "./Chow/Chow.jsx";
